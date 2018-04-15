c050992fd073c8db15f196484c8996189d906806
C L I M H A X <jimchris10@hotmail.fr>
2018-04-15 17:57:08 +0200

    Added MongoDB database

--- ServerApp/app.js
+++ ServerApp/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 var path = require("path");
+var MongoClient = require("mongodb").MongoClient;
+
+MongoClient.connect("mongodb://localhost/tafz", function(error, db) {
+    if (error) return funcCallback(error);
+
+    console.log("Connecté à la base de données 'tutoriel'");
+});
 
 app.use("/", express.static(path.join(__dirname, "/../ClientApp/dist")));
 
