c11c5dd844519f2f4177d1beabb499ab27803250
C L I M H A X <jimchris10@hotmail.fr>
2018-04-15 16:17:42 +0200

    Changed app.js to show index.html

--- ServerApp/app.js
+++ ServerApp/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+var path = require("path");
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use("/", express.static(path.join(__dirname, "/../ClientApp/dist")));
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "/../ClientApp/dist/index.html"))
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
